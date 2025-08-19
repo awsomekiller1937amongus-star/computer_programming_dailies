c90a8fb6bd0dae2faf8883bf78a26f409e5f5d9a
e <awsomekiller1937amongus@gmail.com>
2025-08-19 14:28:54 -0700

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,7 @@
+#writing a function that traverses a list and returns a random name from said list
+#bonus if you write in nested loops for traversal, concatention for result, ect.
+import random
+
+people=(1, 2, 3, 4, 5)
+
+generate_random_people
